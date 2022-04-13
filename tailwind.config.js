6e97bec56be127b3764b55a609177ddb2e9c2695
thenotoriousgustav <guscae4@gmail.com>
2022-04-13 09:39:05 +0700

    portfolio daffa

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#6366f1",
+        dark: "#0f172a",
+        secondary: "#475569",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
